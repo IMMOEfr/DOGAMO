be5c295e1092f9662a1e8948cb29fa03d813f05e
Mohan Francis <121862778+IMMOEfr@users.noreply.github.com>
2024-04-20 11:21:48 +0800

    Update auth.js

--- client/src/auth.js
+++ client/src/auth.js
@@ -5,22 +5,19 @@ import bcrypt from 'bcryptjs-react';
  
 const login = async (credentials) => {
   try {
-    // const reqUser = await fetch(`http://localhost/dogamo/api/index.php?email=${credentials.email}`);
-    // const user = await reqUser.json();
-    // if(user.message == "User not found"){
-    //   throw new Error("Wrong credentials!!");
-    // }
-    // console.log(user.password);
-    // const isPasswordCorrect = await bcrypt.compare(
-    //   credentials.password,
-    //   user.password
-    // );
-    // console.log(isPasswordCorrect);
-    // if (!isPasswordCorrect) throw new Error("Wrong credentials!");
-   const adminPass = "adminpassword";
-   if(adminPass != credentials.password){
-     throw new Error("Wrong credentials");
-   }
+    const reqUser = await fetch(`http://localhost/dogamo/api/index.php?email=${credentials.email}`);
+    const user = await reqUser.json();
+    if(user.message == "User not found"){
+      throw new Error("Wrong credentials!!");
+    }
+    console.log(user.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+    console.log(isPasswordCorrect);
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!");
+   
     return user;
   } catch (error) {
       console.log(error);
